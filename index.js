1850071e9fc92efadbaa42046524f18e2be844c6
Fabiola de la Cruz <itzeel.1504@gmail.com>
2024-11-15 09:35:25 -0600

    thumbnails

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use(express.json());
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 
+app.get('/', (req, res) => {
+    res.redirect('/api/products');
+  });
+
 const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
